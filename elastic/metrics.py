9447619c1db0e88fb898674bbf07915cb2c5427d
giuseppe <giuseppe.mascellaro@mail.polimi.it>
2019-05-23 16:09:43 +0200

    asynchronous throughput computation completed

--- elastic/metrics.py
+++ elastic/metrics.py
@@ -1,15 +1,27 @@
 from elasticsearch import Elasticsearch
 import json
-from abc import abstractmethod
+import random
+from abc import ABC, abstractmethod
 
 
-class Metrics:
-    def __init__(self, conf_path):
+class Metrics(ABC):
+    def __init__(self, conf_path, services_path):
         with open(conf_path) as conf_file:
             conf_data = json.load(conf_file)
         self.es = Elasticsearch(hosts=conf_data['connections'])
         self.index = conf_data['index']
         self.conf_data = conf_data
+        self.services_path = services_path
+
+    def read_services(self):
+        with open(self.services_path) as services_file:
+            services = json.load(services_file)
+        return services['services']
+
+    def format_time_window(self, t0, t1):
+        start_time = t0.strftime('%Y-%m-%dT%H:%M:%S')
+        end_time = t1.strftime('%Y-%m-%dT%H:%M:%S')
+        return end_time, f'@timestamp:[{start_time} TO {end_time}]'
 
     @abstractmethod
     def compute_metric(self):
@@ -23,3 +35,16 @@ class Metrics:
         if query is None:
             query = '*'
         return self.es.search(index=self.index, q=query, size=size)
+
+    def _write(self, operationID, value, name, unit, timestamp):
+        body = {
+            'meter': {
+                'operationID': operationID,
+                'value': value,
+                'name': name,
+                'unit': unit,
+                'timestamp': timestamp
+            }
+        }
+        index = self.index.split('*')[0] + timestamp.split('T')[0]
+        self.es.create(index=index, id=random.randint(0, 99999999), body=body)
--- elastic/throughput.py
+++ elastic/throughput.py
@@ -1,34 +1,48 @@
 import time
+from datetime import datetime
 import threading
+import numpy as np
 from metrics import Metrics
 
 
 class Throughput(Metrics):
-    def __init__(self, conf_path='../conf/conf.json'):
-        super().__init__(conf_path)
+    def __init__(self, conf_path='../conf/conf.json', services_path='../conf/services.json'):
+        super().__init__(conf_path, services_path)
 
     def compute_metric(self, query_content, update_interval):
-        query_ids = query_content + ' AND request.id:*'
         while True:
-            res = self._search(query=query_ids)
-            total_hits = res['hits']['total']
-            res = self._search(query=query_ids, size=total_hits)
-            infos = {}
-            for hit in res['hits']['hits']:
-                id = hit['_source']['request.id']
-                infos[id] = {'response_length': 0, 'request_time': 0}
-            for hit in res['hits']['hits']:
-                source = hit['_source']
-                if 'response.length' in source and 'request.requestTime' in source:
-                    id = source['request.id']
-                    response_length = source['response.length']
-                    request_time = source['request.requestTime']
-                    infos[id]['response_length'] += response_length
-                    infos[id]['request_time'] += request_time * 1e-9
-            for id in infos.keys():
-                throughput = infos[id]['response_length'] / infos[id]['request_time']
-                print(throughput)
+            t0 = datetime.now()
             time.sleep(update_interval)
+            t1 = datetime.now()
+            services = self.read_services()
+            timestamp, time_window = self.format_time_window(t0, t1)
+            timestamp, time_window = '2016-06-20T22:28:46', '[2018-06-20T22:28:46 TO 2020-06-20T22:36:41]'  # TODO: delete this line
+            for service in services:
+                print(service)
+                query_ids = query_content + f' AND request.operationID:{service} AND @timestamp:{time_window}'
+                res = self._search(query=query_ids)
+                total_hits = res['hits']['total']
+                res = self._search(query=query_ids, size=total_hits)
+                throughputs = []
+                infos = {}
+                for hit in res['hits']['hits']:
+                    source = hit['_source']
+                    id = source['request.id']
+                    if id not in infos.keys():
+                        infos[id] = {'response_length': 0, 'request_time': 0}
+                    if 'response.length' in source:
+                        response_length = source['response.length']
+                        infos[id]['response_length'] += response_length
+                    if 'request.requestTime' in source:
+                        request_time = source['request.requestTime']
+                        infos[id]['request_time'] += request_time
+                for id in infos.keys():
+                    throughputs.append((infos[id]['response_length'] / infos[id]['request_time']) * 1e9)
+                throughputs = np.array(throughputs)
+                self._write(operationID=service, value=throughputs.mean(), name='throughputMean', unit='BytesPerSecond', timestamp=timestamp)
+                self._write(operationID=service, value=throughputs.max(), name='throughputMax', unit='BytesPerSecond', timestamp=timestamp)
+                self._write(operationID=service, value=throughputs.min(), name='throughputMin', unit='BytesPerSecond', timestamp=timestamp)
+            print()
 
     def launch_sync_update(self):
         queries = self.conf_data['throughput']['queries']
