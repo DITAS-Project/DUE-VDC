b80134281a87e73dccbf6b651967e49b4182d454
mirco <mirco.manzoni@mail.polimi.it>
2019-09-11 16:38:15 +0200

    data quality

--- rest/data_quality.py
+++ rest/data_quality.py
@@ -1,6 +1,13 @@
 import json
 from flask import Blueprint, request
-import data_quality_metrics as dq
+from rest.data_quality_metrics.accuracy import Accuracy
+from rest.data_quality_metrics.completeness import Completeness
+from rest.data_quality_metrics.volume import Volume
+from rest.data_quality_metrics.consistency import Consistency
+from rest.data_quality_metrics.timeliness import Timeliness
+from rest.data_quality_metrics.precision import Precision
+from copy import deepcopy
+
 
 BP_INTERNAL_STRUCTURE = "INTERNAL_STRUCTURE"
 IN_TESTING_OUTPUT_DATA = 'Testing_Output_Data'
@@ -17,36 +24,41 @@ def compute_data_quality_metrics():
     bp = request.files['bp']
     body = json.load(body)
     bp = json.load(bp)
-    for method in bp[BP_INTERNAL_STRUCTURE][IN_TESTING_OUTPUT_DATA]:
+    # extract method name from http body
+    # non avendo idea di dove troveremo il nome del metodo al momento il sistema funziona con questo falso nome
+    # TODO specificare il metodo per ottenere il nome del metodo della richiesta
+    method_from_http = 'AMethod'
+    print('processing method ' + method_from_http)
+    for method_id in bp[BP_INTERNAL_STRUCTURE][IN_TESTING_OUTPUT_DATA]:
+        if method_id[IN_TOD_METHOD_ID] == method_from_http:
+            method = deepcopy(method_id)
+            break
+    try:
         if IN_TOD_ATTRIBUTES not in method.keys():
             # calcolare tutte le metriche
-            dq.compute_accuracy()
-            dq.compute_completeness()
-            dq.compute_volume()
-            dq.compute_consistency()
-            dq.compute_timeliness()
-            dq.compute_precision()
+            Accuracy().compute_metric(body, bp)
+            Completeness().compute_metric(body, bp)
+            Volume().compute_metric(body, bp)
+            Consistency().compute_metric(body, bp)
+            Timeliness().compute_metric(body, bp)
+            Precision().compute_metric(body, bp)
             pass
         else:
             #calcolare solo metriche indicate in IN_TOD_ATTRIBUTES
-            for attributes in method[IN_TOD_ATTRIBUTES]:
-                if attributes.lower() == 'accuracy':
-                    dq.compute_accuracy()
-                    pass
-                elif attributes.lower() == 'completeness':
-                    dq.compute_completeness()
-                    pass
-                elif attributes.lower() == 'volume':
-                    dq.compute_volume()
-                    pass
-                elif attributes.lower() == 'consistency':
-                    dq.compute_consistency()
-                    pass
-                elif attributes.lower() == 'timeliness':
-                    dq.compute_timeliness()
-                    pass
-                elif attributes.lower() == 'precision':
-                    dq.compute_precision()
-                    pass
-
-    return json.dumps({'msg': 'operation completed!'})
+            for attribute in method[IN_TOD_ATTRIBUTES]:
+                import pdb; pdb.set_trace()
+                if attribute.lower() == 'accuracy':
+                    Accuracy().compute_metric(body, bp)
+                elif attribute.lower() == 'completeness':
+                    Completeness().compute_metric(body, bp)
+                elif attribute.lower() == 'volume':
+                    Volume().compute_metric(body, bp)
+                elif attribute.lower() == 'consistency':
+                    Consistency().compute_metric(body, bp)
+                elif attribute.lower() == 'timeliness':
+                    Timeliness().compute_metric(body, bp)
+                elif attribute.lower() == 'precision':
+                    Precision().compute_metric(body, bp)
+        return json.dumps({'msg': 'operation completed!'})
+    except:
+        return json.dumps({'msg': 'invalid request!'})
---
+++ rest/data_quality_metrics/consistency.py
@@ -0,0 +1,20 @@
+from .data_quality_metric import DataQualityMetric
+
+class Consistency(DataQualityMetric):
+    def __init__(self, conf_path='conf/conf.json'):
+        super().__init__(conf_path)
+
+    def compute_metric(self, body_request, blueprint):
+        print('Now computing consistency!')
+        # TODO compute metrics
+
+        bp_id = '1'
+        vdc_inst = '2'
+        request_id = '3'
+        operation_id = '4'
+        value = '5'
+        name = "precision"
+        unit = '7'
+        hit_timestamp = '8'
+        computation_timestamp = '9'
+        super().write(bp_id, vdc_inst, request_id, operation_id, value, name, unit, hit_timestamp, computation_timestamp)
