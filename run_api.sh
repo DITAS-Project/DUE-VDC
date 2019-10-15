131abf57dbf909a0cc79679b1e8cd6d5807f65f2
giovanni.meroni <giovanni.meroni@polimi.it>
2019-10-15 16:43:14 +0200

    Docker scripts modified to accept external config files.

--- run_api.sh
+++ run_api.sh
@@ -1,2 +1,11 @@
+#!/bin/sh
+
 export FLASK_APP=route.py
-flask run --reload
+
+if [ -f "/etc/ditas/due.json" ]; then
+/bin/cp /etc/ditas/due.json ./conf/conf.json
+fi
+
+ls .
+
+flask run --host=0.0.0.0
