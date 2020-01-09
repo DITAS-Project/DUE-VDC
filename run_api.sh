33732dd832b63ca0dbed16cce4d06d0bd3a53dcc
giovanni.meroni <giovanni.meroni@polimi.it>
2020-01-09 17:27:45 +0100

    Updated shared files

--- run_api.sh
+++ run_api.sh
@@ -4,6 +4,8 @@ export FLASK_APP=route.py
 
 if [ -f "/etc/ditas/due.json" ]; then
 /bin/cp /etc/ditas/due.json ./conf/conf.json
+/bin/cp /etc/ditas/vdc-id ./conf/vdc-id
+/bin/cp /etc/ditas/blueprint.json ./conf/blueprint.json
 fi
 
 ls .
