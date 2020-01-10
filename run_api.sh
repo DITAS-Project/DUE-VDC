9c0bbcb25e9be622a40a969dd6e31b881b34b261
giovanni.meroni <giovanni.meroni@polimi.it>
2020-01-10 11:52:21 +0100

    Changed config files

--- run_api.sh
+++ run_api.sh
@@ -4,7 +4,6 @@ export FLASK_APP=route.py
 
 if [ -f "/etc/ditas/due.json" ]; then
 /bin/cp /etc/ditas/due.json ./conf/conf.json
-/bin/cp /etc/ditas/vdc-id ./conf/vdc-id
 /bin/cp /etc/ditas/blueprint.json ./conf/blueprint.json
 fi
 
